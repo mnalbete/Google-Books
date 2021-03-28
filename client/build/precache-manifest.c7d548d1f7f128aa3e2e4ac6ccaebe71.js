c70111fe9b3a40bd2f6ebfa47fbbbdef6c776240
mathew <mnalbete@uw.edu>
2021-03-27 18:35:11 -0700

    added npm

---
+++ client/build/precache-manifest.c7d548d1f7f128aa3e2e4ac6ccaebe71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c62b5b9ece96952e0e03",
+    "url": "/static/css/main.4a926637.chunk.css"
+  },
+  {
+    "revision": "c62b5b9ece96952e0e03",
+    "url": "/static/js/main.05fef6db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21dd6e3f9805d001fa42",
+    "url": "/static/js/2.45fe7edf.chunk.js"
+  },
+  {
+    "revision": "4b06c06d25ee2cca2b2b3480ed357fb0",
+    "url": "/index.html"
+  }
+];
